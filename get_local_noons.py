b94a8e4cf434d7f3b9161ea369f95545f77678b6
John Morgan <john.morgan@icrar.org>
2020-06-30 09:43:01 +0800

    switch to python3 and use datetime

--- get_local_noons.py
+++ get_local_noons.py
@@ -1,4 +1,5 @@
-from pyyaml import safe_load
+from yaml import safe_load
+from datetime import datetime
 from astropy.time import Time
 from astropy.coordinates import EarthLocation, Longitude
 from astroplan import Observer
@@ -12,16 +13,16 @@ location = EarthLocation.from_geodetic(lat=conf['lat']*u.deg,
                                        height=conf['alt']*u.m)
 obs = Observer(location)
 
-t = Time(conf['startDate'] + 12*u.hour - conf['timezone']*u.hour)
-t_end = Time(conf['stopDate'] + 12*u.hour - conf['timezone']*u.hour)
+t = Time(datetime.combine(conf['startDate'], datetime.min.time()))  + 12*u.hour - conf['timezone']*u.hour
+t_end = Time(datetime.combine(conf['stopDate'], datetime.min.time()))  + 12*u.hour - conf['timezone']*u.hour
 
 with open(conf['files']['noon'], 'w') as f:
-    print >> f, "local_noon_str,local_noon_lst"
+    print("local_noon_str,local_noon_lst", file=f)
     while t <= t_end:
         local_noon_str = obs.noon(t, which='nearest').isot
         local_noon_lst = Longitude(obs.local_sidereal_time(local_noon_str), wrap_angle=180*u.deg).deg
-        print local_noon_str,
-        print local_noon_lst
-        print
-        print >> f, "%s,%.3f" % (local_noon_str[:19],local_noon_lst)
+        print(local_noon_str, end=' ')
+        print(local_noon_lst)
+        print()
+        print("%s,%.3f" % (local_noon_str[:19],local_noon_lst), file=f)
         t += INTERVAL*u.day
