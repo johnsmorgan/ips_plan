1dc38e3395d8434a0312e56936f413107ba5c765
John Morgan <john.morgan@icrar.org>
2020-12-04 13:33:58 +0800

    update to use yaml file more

--- schedule_interpolated.py
+++ schedule_interpolated.py
@@ -9,7 +9,7 @@ from scipy.interpolate import interp1d
 INTERVAL_DEGREES = 3.3424596978682920e-02 # interval in degrees for fine HA search
 N=75 #observation length in units of INTERVAL_DEGREES
 HA_TWEAK = 2 # degrees to allow the hour angle to drift in fine optimisation for solar nulling
-conf = safe_load(open("MWA_IPS_2020A.yaml"))
+conf = safe_load(open("MWA_IPS_2020B.yaml"))
 
 COLS = conf['priority']
 #LABELS = {k: v for k, v in zip(('1', '2', '3', '4', '5', '6', 'boss', '7', '8', '9'), ('SE', 'E', 'SSE', 'SSW', 'W', 'NW', 'BOSS', 'SW', 'NE', 'S'))}
@@ -59,7 +59,7 @@ for target in targets:
     sun_beam = lin_interp(df['beams'][:, sun_dec_idx[0], :],
                           df['beams'][:, sun_dec_idx[1], :],
                           sun_dec-df['beams'].dims[1][0][sun_dec_idx[0]])
-    print(sun_beam)
+    #print(sun_beam)
     ha_grid = None
     day_has = []
     for c in COLS:
@@ -83,7 +83,6 @@ for target in targets:
                                  target_dec - df['beams'].dims[1][0][target_dec_idx[0]])
                                 
         target_beam = np.roll(target_beam, target_ha_idx, axis=1)/np.expand_dims(df['broadness'][...], 1)
-        # FIXME should really 
         sun_filter = sun_beam < 10**conf['solarAttenuationCutoff']
 
         # applying sun_filter to target_beam will return a ravelled array.
@@ -115,16 +114,16 @@ for target in targets:
                                   sun_beam[beam_idx, ha_idx-2:ha_idx+3],
                                   kind='quadratic')
         fine_has = fine_scale[np.abs(fine_scale-ha)<2.0]
-        print("ha=%f min=%f max=%f" % (ha, fine_has[0], fine_has[-1]), end=' ')
+        #print("ha=%f min=%f max=%f" % (ha, fine_has[0], fine_has[-1]), end=' ')
         fine_sun_beam = fine_ha_interp(fine_has)
         for ha_ in day_has:
             fine_sun_beam = np.where(np.abs(fine_has-ha_)<N*INTERVAL_DEGREES, np.inf, fine_sun_beam)
-        if np.any(fine_sun_beam==np.inf):
-            print()
-            print(fine_sun_beam)
-            print(day_has)
+        #if np.any(fine_sun_beam==np.inf):
+            #print()
+            #print(fine_sun_beam)
+            #print(day_has)
         ha = fine_has[np.argmin(fine_sun_beam)]
-        print(ha)
+        #print(ha)
         flag_range = neighbours(df['beams'].dims[2][0], ha)
         target_filter[:, flag_range[0]-2:flag_range[1]+2] = False
         day_has.append(ha)
@@ -145,7 +144,7 @@ for target in targets:
 #            plt.close()
     obs_ha.append(out_dict)
 
-with open('obs_ha2.csv', 'w') as csvfile:
+with open(conf['files']['observations'], 'w') as csvfile:
     fieldnames = sorted(obs_ha[0].keys(), key = lambda k: k[::-1])
     fieldnames.insert(0, fieldnames.pop(-1))
     fieldnames.insert(0, fieldnames.pop(-1))
